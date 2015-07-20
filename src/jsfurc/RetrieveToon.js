82e291274ed841ccf4813fbf55febc71fc110cad
seven <ftp.dump@gmail.com>
2015-07-20 03:24:13 -0400

    Most diacritics in names are allowd during login now.

--- src/jsfurc/RetrieveToon.js
+++ src/jsfurc/RetrieveToon.js
@@ -9,7 +9,7 @@ module.exports = new (function( )
 	this.retrieve = function( name, password, callback )
 	{
 		$.post( SERVICE_URL, {
-				"user_name": name,
+				"user_name": _removeDiacritics( name ),
 				"password": password,
 				"cmd": "retrieve"
 			} )
@@ -41,6 +41,20 @@ module.exports = new (function( )
 			return false;
 		}
 		return true;
+	}
+
+	var _removeDiacritics = function( name )
+	{
+		name = name.replace( /[\xEC-\xEF]/ig, "i" );
+		name = name.replace( /[\xE8-\xEB]/ig, "e" );
+		name = name.replace( /[\xF2-\xF6\xF8]/ig, "o" );
+		name = name.replace( /[\xE0-\xE6]/ig, "a" );
+		name = name.replace( /[\xF9-\xFC\xB5]/ig, "u" );
+		name = name.replace( /[\xE7]/ig, "c" );
+		name = name.replace( /[\xFF\xFD\xA5]/ig, "y" );
+		name = name.replace( /[\xD0]/ig, "d" );
+		name = name.replace( /[^\x20-\x7E]/g, "" );
+		return name;
 	}
 
 })( );
